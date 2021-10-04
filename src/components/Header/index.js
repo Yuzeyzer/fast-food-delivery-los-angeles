0598daca53197b5ce3aa1eb9d37a7043a62a9ac0
John Doe <kurbanof.atabek@gmail.com>
2021-10-05 03:21:29 +0600

    projec basic structure

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.scss';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__container">
+        <a href="" className="logo">
+        Constructor
+        </a>
+      </div>
+    </header>
+  )
+}
+
+export default Header
