252f14ae78a7313a08656e6ed6e9cfae860a1930
John Doe <kurbanof.atabek@gmail.com>
2021-10-22 03:20:07 +0600

    created authProvider and useSignup hook

---
+++ src/hooks/useSignup.js
@@ -0,0 +1,16 @@
+import { useCallback } from 'react'
+import { auth } from '@/firebase/config'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+
+const useSignup = useCallback(async (email, password) => {
+	let user
+	let error
+	try {
+		user = await createUserWithEmailAndPassword(auth, email, password)
+	} catch (err) {
+		error = err.message
+	}
+	return { user, error }
+})
+
+export default useSignup
